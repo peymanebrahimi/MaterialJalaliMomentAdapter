e66d8aa1a96daf6dd27c99f8a7ab43e46c37721e
Peyman Ebrahimi <peyman.ebrahimi@gmail.com>
2020-08-25 11:25:40 +0430

    use simple func instead of arrow one

---
+++ projects/material-jalali-moment-adapter/src/lib/adaptor/index.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { JalaliMomentDateAdapter } from './jalali-moment-date-adapter';
+import { DateAdapter, MAT_DATE_LOCALE, MAT_DATE_FORMATS } from '@angular/material/core';
+import { MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
+import { JALALI_MOMENT_FORMATS, MOMENT_FORMATS } from './jalali_moment_formats';
+
+export * from './jalali-moment-date-adapter';
+export * from './jalali_moment_formats';
+
+
+@NgModule({
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: JalaliMomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
+    }
+  ],
+})
+export class JalaliMomentDateModule { }
+
+
+@NgModule({
+  imports: [JalaliMomentDateModule],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: "fa" }, // en-GB  fr
+    {
+      provide: MAT_DATE_FORMATS,
+      useFactory: formatFactory,
+      deps: [MAT_DATE_LOCALE]
+      // useValue: JALALI_MOMENT_FORMATS
+    }
+  ],
+})
+export class MaterialJalaliMomentAdapterModule { } 
+
+export function formatFactory(locale: string) {
+  if (locale === "fa") {
+    return JALALI_MOMENT_FORMATS;
+  } else {
+    return MOMENT_FORMATS;
+  }
+}
+
+/*
+      useFactory: (locale: string) => {
+        if (locale === "fa") {
+          return JALALI_MOMENT_FORMATS;
+        } else {
+          return MOMENT_FORMATS;
+        }
+      },
+*/
--- projects/material-jalali-moment-adapter/src/public-api.ts
+++ projects/material-jalali-moment-adapter/src/public-api.ts
@@ -1,7 +1,1 @@
-/*
- * Public API Surface of material-jalali-moment-adapter
- */
-
-export * from './lib/material-jalali-moment-adapter.service';
-export * from './lib/material-jalali-moment-adapter.component';
-export * from './lib/material-jalali-moment-adapter.module';
+export * from './lib/adaptor/index';
