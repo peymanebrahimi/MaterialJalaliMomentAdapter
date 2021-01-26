313898cc43284a1129dde4510c6a20b671cae8ef
peymanebrahimi <peyman.ebrahimi@gmail.com>
2021-01-26 21:21:10 +0330

    ng update11.1.1

--- projects/jalaliAdapterUsage/src/app/app.component.spec.ts
+++ projects/jalaliAdapterUsage/src/app/app.component.spec.ts
@@ -20,16 +20,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'jalaliAdapterUsage'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('jalaliAdapterUsage');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('jalaliAdapterUsage app is running!');
-  });
 });
